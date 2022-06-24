d1bbaa7bd92d02699601816374c297b8523f2dd4
adnan <adnan172203@gmail.com>
2022-06-24 18:21:00 +0600

    database connection config changed

--- auth/server.js
+++ auth/server.js
@@ -6,23 +6,6 @@ const dotenv = require('dotenv');
 
 var app = express();
 
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-  }
-);
-
-sequelize
-  .authenticate()
-  .then(() => console.log('Connection has been established successfully.'))
-  .catch((err) => console.log('error===>>>>>>', err));
-
 // Load env vars
 dotenv.config({ path: './config/.env' });
 
